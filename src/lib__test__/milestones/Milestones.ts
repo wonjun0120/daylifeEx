b65ac1f0c1cb8deac7002ce02bfc7fc5a66ecbb9
최원준 <wonjundero@naver.com>
2020-09-14 23:52:20 +0900

    Change seqeulize version 6->5, Add assosiations

--- src/lib__test__/milestones/Milestones.ts
+++ src/lib__test__/milestones/Milestones.ts
@@ -7,16 +7,23 @@
  * end: varchar
  * priority: int
  */
-import {Model, Table, Column, HasMany} from 'sequelize-typescript';
+import {Model, Table, Column, HasMany, ForeignKey} from 'sequelize-typescript';
+import { User } from '../../lib/users/Users';
+import { Todo } from '../../lib/todos/Todos';
 
 @Table({
     timestamps: false,  //createdAt updatedAt은 사용하지 않음
 })
 export class Milestone extends Model<Milestone> {
+    @ForeignKey(() => User)
     @Column user_id!: number;
+
     @Column name!: string;
     @Column memo!: string;
     @Column start!: string;
     @Column end!: string;
     @Column priority!: number;
+
+    @HasMany(() => Todo)
+    todos!: Todo[];
 } 
