f39b361792c2d0f95a1e19c0c0bf33751e1bfd53
최원준 <wonjundero@naver.com>
2020-09-14 23:16:23 +0900

    Add config, Add Test DB

---
+++ src/lib__test__/database/sequelize.ts
@@ -0,0 +1,17 @@
+import {Sequelize} from 'sequelize-typescript';
+
+// import {Milestone} from '../milestones/Milestones'
+// import {Schedule} from '../schedules/Schedules'
+// import {Todo} from '../todos/Todos'
+// import {User} from '../users/Users'
+import { env } from 'process';
+
+export const seqeulize = new Sequelize({
+    database: 'daylifeTest',
+    dialect: 'mysql',
+    username: 'root',
+    password: env.DATABASE_DEV_PASSWORD,
+    storage: ':memory:',
+    models: [__dirname + '/models'],
+    repositoryMode: true,
+})
